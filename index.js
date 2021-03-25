1557f2662daa3c2fd627082cb50a2fb4bf828d83
Sami <samialeksi.tuomela@gmail.com>
2021-03-25 19:06:43 +0200

    added start

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 app.use(express.static('build'))
+
+
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -15,8 +19,7 @@ app.use(cors())
 app.use(express.json())
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://fullstack:stackfull@cluster0.kkhvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -36,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
