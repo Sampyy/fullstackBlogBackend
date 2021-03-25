4cdcbf80e065d0c6a63ba95fad4da6b43f2faaf3
Sami <samialeksi.tuomela@gmail.com>
2021-03-25 19:50:29 +0200

    4.1 done

--- index.js
+++ index.js
@@ -6,38 +6,24 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 app.use(express.static('build'))
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
 
 
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+
 app.use(cors())
 app.use(express.json())
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to mongoDB: ', error.message)
+  })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
