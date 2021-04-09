33616bb713490811cc3c5882486fa09690462a0b
Sami <samialeksi.tuomela@gmail.com>
2021-04-09 19:05:14 +0300

    did 4.3, 4.4

---
+++ utils/list_helper.js
@@ -0,0 +1,17 @@
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (likes, blog) => {
+        return likes + blog.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
