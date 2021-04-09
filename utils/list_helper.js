93e1a3882f9378706a1a843098e3e88db3c4ef88
Sami <samialeksi.tuomela@gmail.com>
2021-04-09 22:54:55 +0300

    added 4.5, 4.6, 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,4 @@
+const blog = require("../models/blog")
 
 const dummy = (blogs) => {
     return 1
@@ -11,7 +12,53 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    var currentFavorite = blogs[0]
+    blogs.map((blog) => {
+        if (blog.likes > currentFavorite.likes) {
+            currentFavorite = blog
+        }
+    })
+
+    return currentFavorite || "No blogs"
+}
+
+const mostBlogs = (blogs) => {
+    var currentTopAuthor = ""
+    var currentTopBlogs = 0
+    var blogsAmounts = {}
+
+    blogs.map((blog) => {
+        blogsAmounts[blog.author] = blogsAmounts[blog.author] + 1 || 1
+        if (blogsAmounts[blog.author] > currentTopBlogs) {
+            currentTopAuthor = blog.author
+            currentTopBlogs = blogsAmounts[blog.author]
+        }
+    })
+
+    return {"author":currentTopAuthor, "blogs":Number(currentTopBlogs)}
+}
+
+const mostLikes = (blogs) => {
+    var likesAmounts = {}
+    var currentTopAuthor=""
+    var currentTopLikes = 0
+
+    blogs.map((blog) => {
+        likesAmounts[blog.author] = likesAmounts[blog.author] + blog.likes || blog.likes
+        if (likesAmounts[blog.author] > currentTopLikes) {
+            currentTopAuthor = blog.author
+            currentTopLikes = likesAmounts[blog.author]
+        }
+    })
+
+    return {"author": currentTopAuthor, "likes": currentTopLikes}
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
