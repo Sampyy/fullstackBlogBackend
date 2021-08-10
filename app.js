dc4d6447dc3e99ac34fc178d2bede0f6f9be80b6
Sami <samialeksi.tuomela@gmail.com>
2021-08-10 14:16:06 +0300

    split index.js and app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to mongoDB: ', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
+const app = require('./app')
 const http = require('http')
-const cors = require('cors')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
 
 
-app.use(express.static('build'))
+const server = http.createServer(app)
 
 
-
-
-
-
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to mongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to mongoDB: ', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
